8945f939f78c887dcb457a966f7507790dae3d90
python-learner123 <61101396+python-learner123@users.noreply.github.com>
2020-07-05 21:44:51 +0530

    Create WebTest.go

---
+++ WebTest.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is %s !", r.URL.Path[1:]) //Fprintf to open in page
+
+}
+
+func main() {
+
+	http.HandleFunc("/view/", handle)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
