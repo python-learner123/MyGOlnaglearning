06126e5557cc7be569ef5ebdf359de31a3d989b0
Yogesh_Patel <61101396+python-learner123@users.noreply.github.com>
2020-07-27 12:58:27 +0530

    Update Basic_Function.go

--- Basic_Function.go
+++ Basic_Function.go
@@ -2,7 +2,10 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func calculate(fi int, sec int) (int, int) {
 	sum := fi + sec
@@ -11,16 +14,30 @@ func calculate(fi int, sec int) (int, int) {
 	return sum, diff
 }
 func fizbuzz(num int) {
+	//myfizbuzzslice := make([]string, 1)
+	var myfizbuzzslice []string
 	if num%5 == 0 && num%3 == 0 {
-		fmt.Println("FizBuzz")
-	} else if num%5 == 0 {
-		fmt.Println("Fiz")
+		//fmt.Println("FizBuzz")
+		myfizbuzzslice = append(myfizbuzzslice, "FizBuzz")
 	} else if num%3 == 0 {
-		fmt.Println("Buzz")
+		//fmt.Println("Fiz")
+		myfizbuzzslice = append(myfizbuzzslice, "Fiz")
+	} else if num%5 == 0 {
+		//fmt.Println("Buzz")
+		myfizbuzzslice = append(myfizbuzzslice, "Buzz")
 	} else {
-		fmt.Println(num)
+		//fmt.Println(num)
+		myfizbuzzslice = append(myfizbuzzslice, strconv.Itoa(num))
+	}
+	myfizbuzz_Result(myfizbuzzslice)
+}
+
+func myfizbuzz_Result(s []string) {
+	for _, value := range s {
+		fmt.Println(value)
 	}
 }
+
 func main() {
 
 	x, y := 100, 50
@@ -28,7 +45,9 @@ func main() {
 	fmt.Println("sum is :===", summ, "\n", "difference is :===", diiff)
 
 	//Fiz Buzz test function
+
 	for i := 1; i <= 30; i++ {
 		fizbuzz(i)
 	}
+
 }
